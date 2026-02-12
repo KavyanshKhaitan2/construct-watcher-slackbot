bd7d5b7d385494537be044194694cba91026cbb3
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-02-12 13:31:39 +0530

    updated

--- app.py
+++ app.py
@@ -15,19 +15,27 @@ from construct_sdk.get_user_data import get_user_data, get_user_data_from_slack_
 # This sample slack application uses SocketMode
 # For the companion getting started setup guide,
 # see: https://docs.slack.dev/tools/bolt-python/getting-started
+TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+dotenv.load_dotenv()
+DEBUG = os.environ.get("DEBUG", 'false').lower() in ['true', '1', 'on']
 
-token = os.environ.get("SLACK_BOT_TOKEN")
-if token is None:
-    dotenv.load_dotenv()
-    token = os.environ.get("SLACK_BOT_TOKEN")
+if DEBUG:
+    print("---> DEBUG is on!")
+
+if TOKEN is None:
+    TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 
 # Initializes your app with your bot token
-app = App(token=token)
+app = App(token=TOKEN)
 # app.client.chat_postMessage(channel="#kavyansh", text="Back online!")
 
 with open("allowed_channels.txt") as f:
     channel_list = f.read().split('\n')
 
+def command(command):
+    if DEBUG:
+        return app.command('/dev-'+command)
+    return app.command('/'+command)
 
 # Listens to incoming messages that contain "hello"
 @app.message("!hello")
@@ -53,7 +61,7 @@ def message_hello(message, say: Say):
 
     global channel_list
     with open("allowed_channels.txt") as f:
-        channel_list = f.readlines()
+        channel_list = f.read().split('\n')
 
     say(
         text=f"Also, these are the channels I am allowed to reply in:\n{channel_list}",
@@ -61,29 +69,14 @@ def message_hello(message, say: Say):
     )
 
 
-# @app.message("goodbye")
-# def message_goodbye(say: Say):
-#     responses = ["Adios", "Au revoir", "Farewell"]
-#     parting = random.choice(responses)
-#     say(f"{parting}!")
-
-
-# @app.action("button_click")
-# def action_button_click(body, ack, say: Say):
-#     # Acknowledge the action
-#     ack()
-#     say(f"<@{body['user']['id']}> clicked the button")
-
-
 GOAL = 42  # hours
 
-
-@app.command("/construct-time")
+@command("construct-time")
 def construct_time(ack, say: Say, command):
     ack()
     print("acked request for time")
     slack_user_id = command["user_id"]
-    res = say(f"<@{slack_user_id}> ran:\n/construct_time")
+    res = say(f"<@{slack_user_id}> ran:\n/construct-time")
     thread_ts = res["ts"]
     say = Say(
         client=say.client,
@@ -93,13 +86,19 @@ def construct_time(ack, say: Say, command):
         build_metadata=say.build_metadata,
     )
     try:
+        message = res
+        if message["channel"] not in channel_list:
+            say(
+                ":no-no: Please only use this bot in #construct-watcher-stats!\n\n",
+            )
+            return
         say(f"{slack_user_id}")
         print(f"{command}")
-        message = res
         print(f"[INFO] Received command: {message}")
         print(type(message))
+        start = time.time()
         user_data = get_user_data_from_slack_id(slack_user_id)
-        say(f"Please wait, fetching all your projects! _(estimated time: {1.5 * len(user_data['projects'])}s)_")
+        say(f"Please wait, fetching all your projects! _(estimated time: {round((time.time()-start) * len(user_data['projects']), 2)}s)_")
         start = time.time()
         projects = []
         for project in user_data["projects"]:
@@ -164,14 +163,37 @@ def construct_time(ack, say: Say, command):
         ]
         total_time = 0
         total_redeemable_time = 0
+        total_clay_time = user_data['requestedUser']['clay'] * 60
         for project in projects:
-            project_name = project["project"]["name"]
+            max_length = 20
+
+            project_name:str = project["project"]["name"]
+            if len(project_name) > max_length+3:
+                project_name = project_name[:max_length]+"..."
             project_time_spent = int(project["project"]["timeSpent"])
             total_time += project_time_spent
-            if project_time_spent > 120:
+            if project_time_spent >= 120:
                 total_redeemable_time += project_time_spent
             hours = project_time_spent // 60
             mins = project_time_spent % 60
+            if project['project']['status'] == 'finalized':
+                last_col_text = "✅ Finalized"
+            elif project['project']['status'] == 'printed':
+                total_clay_time += project_time_spent
+                last_col_text = "🖨️ Printed"
+            elif project['project']['status'] == 'printing':
+                total_clay_time += project_time_spent
+                last_col_text = "⏱️ Printing"
+            elif project['project']['status'] == 'submitted':
+                total_clay_time += project_time_spent
+                last_col_text = "🔃 Submitted"
+            elif project['project']['status'] == 'rejected':
+                last_col_text = "❌ Rejected"
+            elif hours >= 2:
+                total_clay_time += project_time_spent
+                last_col_text = "👍 Redeemable"
+            else:
+                last_col_text = "🚧 Building"
             table_rows.append(
                 [
                     {
@@ -198,7 +220,7 @@ def construct_time(ack, say: Say, command):
                             {
                                 "type": "rich_text_section",
                                 "elements": [
-                                    {"type": "text", "text": "⏲️" if hours < 2 else " "}
+                                    {"type": "text", "text": last_col_text}
                                 ],
                             }
                         ],
@@ -213,6 +235,10 @@ def construct_time(ack, say: Say, command):
         hours = total_redeemable_time // 60
         mins = total_redeemable_time % 60
         daily_reedemable_time_formatted = f"{hours}h {mins}m"
+        
+        hours = int(total_clay_time // 60)
+        mins = round(total_clay_time % 60)
+        total_clay_time_formatted = f"{hours}h {mins}m"
         
         table_rows += [
             [
@@ -301,7 +327,54 @@ def construct_time(ack, say: Say, command):
                             "elements": [
                                 {
                                     "type": "text",
-                                    "text": "⏲️",
+                                    "text": " ",
+                                    "style": {"bold": True},
+                                }
+                            ],
+                        }
+                    ],
+                },
+            ],
+            [
+                {
+                    "type": "rich_text",
+                    "elements": [
+                        {
+                            "type": "rich_text_section",
+                            "elements": [
+                                {
+                                    "type": "text",
+                                    "text": "Clay time",
+                                    "style": {"bold": True},
+                                }
+                            ],
+                        }
+                    ],
+                },
+                {
+                    "type": "rich_text",
+                    "elements": [
+                        {
+                            "type": "rich_text_section",
+                            "elements": [
+                                {
+                                    "type": "text",
+                                    "text": total_clay_time_formatted,
+                                    "style": {"bold": True},
+                                }
+                            ],
+                        }
+                    ],
+                },
+                {
+                    "type": "rich_text",
+                    "elements": [
+                        {
+                            "type": "rich_text_section",
+                            "elements": [
+                                {
+                                    "type": "text",
+                                    "text": " ",
                                     "style": {"bold": True},
                                 }
                             ],
@@ -315,25 +388,25 @@ def construct_time(ack, say: Say, command):
         time_left = deadline - datetime.date.today()
         days_left = time_left.days
         
-        total_time = (GOAL - total_time) / days_left
-        total_redeemable_time = (GOAL - total_redeemable_time) / days_left
+        total_time_for_calc = (GOAL - total_time) / days_left
+        total_clay_time_for_calc = (GOAL - total_clay_time) / days_left
         
-        hours = int(total_time // 60)
-        mins = int(total_time % 60)
+        hours = int(total_time_for_calc // 60)
+        mins = int(total_time_for_calc % 60)
         daily_time_formatted = f"{hours}h {mins}m"
         
-        hours = int(total_redeemable_time // 60)
-        mins = int(total_redeemable_time % 60)
-        daily_reedemable_time_formatted = f"{hours}h {mins}m"
+        hours = int(total_clay_time_for_calc // 60)
+        mins = int(total_clay_time_for_calc % 60)
+        daily_clay_time_formatted = f"{hours}h {mins}m"
         
         blocks = [{"type": "table", "rows": table_rows}, {
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": f"Deadline in *{days_left}* days ({deadline})\nGOAL: {GOAL/60} hours\nDo *{daily_time_formatted}* every day to reach GOAL.\nDo *{daily_reedemable_time_formatted}* every day to reach GOAL. *(calculated using redeemable time)*",
+				"text": f"Deadline in *{days_left}* days ({deadline})\nGOAL: {GOAL/60} hours\nDo *{daily_time_formatted}* every day to reach GOAL.\nDo *{daily_clay_time_formatted}* every day to reach GOAL. *(calculated using clay time)*",
 			}
 		}]
-        say(blocks=blocks)
+        say(blocks=blocks, text="Your construct report has been generated.")
     except Exception as _:
         traceback_text = traceback.format_exc()
         say(
@@ -347,7 +420,7 @@ def construct_time(ack, say: Say, command):
         )
 
 
-@app.command("/construct-user-info")
+@command("construct-user-info")
 def construct_user_info(ack, say: Say, command):
     # Acknowledge command request
     ack()
@@ -405,6 +478,7 @@ def construct_user_info(ack, say: Say, command):
             # f"T2 reviewer? {found['hasT2Review']}\n"
             # f"Admin? {found['hasAdmin']}\n"
             f"*Market score:* {found['shopScore']}\n"
+            f"*Balance:* *{round(found['clay'], 2)}* clay, *{found['brick']}* bricks.\n"
             f"*Total time logged:* {formatted_time}\n"
             f"*Number of devlogs:* {devlog_count}\n"
         )
